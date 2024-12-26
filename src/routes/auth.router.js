dce81bb0f1a6cfd48e8ea8d7d2e95e5b59804d9c
Luis Casas <luisscasas45@gmail.com>
2024-12-26 13:52:37 -0600

    FEAT/ remove unnecessary integrations

--- src/routes/auth.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userRouter = require('./users.router');
-
-router.use('/users', userRouter);
-
-module.exports = router;
